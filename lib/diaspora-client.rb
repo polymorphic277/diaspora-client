9899cc6b632b713dccea0b6061f56b44e6fb66c2
Ilya Zhitomirskiy <ilya@laptop.(none)>
2011-06-14 15:47:20 -0700

    green

--- lib/diaspora-client.rb
+++ lib/diaspora-client.rb
@@ -6,5 +6,28 @@ module DiasporaClient
   autoload :App,            File.join('diaspora-client', 'app')
   autoload :AccessToken,    File.join('diaspora-client', 'access_token')
   autoload :ResourceServer, File.join('diaspora-client', 'resource_server')
+
+
+  def self.private_key_path
+    @private_key_path
+  end
+
+  def self.private_key_path= path
+    @private_key_path = path
+  end
+
+  def self.initialize_instance_variables
+    @private_key_path = nil
+    if defined?(Rails) && !Rails.root.nil?
+      @private_key_path = Rails.root + "/config/private.pem" 
+    end
+
+    @private_key = nil
+  end
+
+  def self.private_key
+    @private_key ||= OpenSSL::PKey::RSA.new(File.read(@private_key_path))
+  end
+  self.initialize_instance_variables
 end
 
--- lib/diaspora-client/resource_server.rb
+++ lib/diaspora-client/resource_server.rb
@@ -6,23 +6,34 @@ module DiasporaClient
       self_url = "#{self_url.host}:#{self_url.port}" if self_url.respond_to?(:host)
       pod = self.new(:host => host)
 
-      if defined?(EM::Synchrony) && EM.reactor_running?
-        connection = Faraday::Connection.new do |builder|
-            builder.use Faraday::Adapter::EMSynchrony
-        end
-      else
-        connection = Faraday.default_connection
+      connection = Faraday::Connection.new do |builder|
+        builder.use Faraday::Adapter::EMSynchrony  if defined?(EM::Synchrony) && EM.reactor_running?
       end
 
+      signable_string = pod.signable_string(self_url)
       response = connection.post("http://#{host}/oauth/token",
                                   {:type => :client_associate,
-                                   :manifest_url =>"http://#{self_url}/manifest.json"})
+                                   :manifest_url =>"http://#{self_url}/manifest.json",
+                                   :signed_string => signable_string,
+                                   :signature => pod.signature(signable_string)})
 
       json = JSON.parse(response.body)
       pod.update_attributes(json)
       pod.save!
       pod
     end
+
+    def signable_string(self_url)
+      [ "http://#{self_url}/",
+        "http://#{self.host}/",
+        Time.now.to_i,
+        ActiveSupport::SecureRandom.base64(32)
+      ].join(';')
+    end
+
+   def signature(plaintext)
+     DiasporaClient.private_key.sign( OpenSSL::Digest::SHA256.new, plaintext)
+   end
 
     def client
       @client ||= OAuth2::Client.new(client_id, client_secret, :site => "http://#{host}/api/v0")
